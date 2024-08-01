6b743af17100304c207731c6e0d19e242f2b3f28
Kamil Tomaszewski <ktomaszewski@qualtrics.com>
2024-08-01 16:30:38 +0200

    Add GH Pages modifications

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/simple-react-ecommerce">
       <App />
     </BrowserRouter>
   </React.StrictMode>
