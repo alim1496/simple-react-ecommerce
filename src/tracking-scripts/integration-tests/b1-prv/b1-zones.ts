2662cd8804574740575bc4742f7512d5e86ea636
Ilya <ilyak@qualtrics.com>
2024-11-07 16:07:38 +0100

    g1

---
+++ src/tracking-scripts/integration-tests/b1-prv/b1-zones.ts
@@ -0,0 +1,17 @@
+import ZN_1pWMfiB7kzLU9UD from "./ZN_1pWMfiB7kzLU9UD";
+import ZN_2EyJTvRl0kjxmgC from "./ZN_2EyJTvRl0kjxmgC";
+import ZN_2YnYLD0FLPY7yKv from "./ZN_2YnYLD0FLPY7yKv";
+import ZN_3DqJL8DLS52pfUQ from "./ZN_3DqJL8DLS52pfUQ";
+import ZN_3PbE4oFVtHVf9eP from "./ZN_3PbE4oFVtHVf9eP";
+import ZN_bmfmN4BHs2iI3v3 from "./ZN_bmfmN4BHs2iI3v3";
+
+const b1ZoneToScript: Record<string, string> = {
+    'ZN_1pWMfiB7kzLU9UD': ZN_1pWMfiB7kzLU9UD,
+    'ZN_2EyJTvRl0kjxmgC': ZN_2EyJTvRl0kjxmgC,
+    'ZN_2YnYLD0FLPY7yKv': ZN_2YnYLD0FLPY7yKv,
+    'ZN_3DqJL8DLS52pfUQ': ZN_3DqJL8DLS52pfUQ,
+    'ZN_3PbE4oFVtHVf9eP': ZN_3PbE4oFVtHVf9eP,
+    'ZN_bmfmN4BHs2iI3v3': ZN_bmfmN4BHs2iI3v3
+};
+
+export default b1ZoneToScript;
---
+++ src/tracking-scripts/integration-tests/g1-cmh/g1-zones.ts
@@ -0,0 +1,17 @@
+import ZN_3qWoW3yt7WSMx45 from "./ZN_3qWoW3yt7WSMx45";
+import ZN_1Qo1XHrjVMt9WKK from "./ZN_1Qo1XHrjVMt9WKK";
+import ZN_UcCfn0eFJLBMS4h from "./ZN_UcCfn0eFJLBMS4h";
+import ZN_3JKGER1ayOuqUN0 from "./ZN_3JKGER1ayOuqUN0";
+import ZN_27pWWnGTaa2g8a1 from "./ZN_27pWWnGTaa2g8a1";
+import ZN_qLw4EhlxD0JXNU5 from "./ZN_qLw4EhlxD0JXNU5";
+
+const g1ZoneToScript: Record<string, string> = {
+    'ZN_3qWoW3yt7WSMx45': ZN_3qWoW3yt7WSMx45,
+    'ZN_1Qo1XHrjVMt9WKK': ZN_1Qo1XHrjVMt9WKK,
+    'ZN_UcCfn0eFJLBMS4h': ZN_UcCfn0eFJLBMS4h,
+    'ZN_3JKGER1ayOuqUN0': ZN_3JKGER1ayOuqUN0,
+    'ZN_27pWWnGTaa2g8a1': ZN_27pWWnGTaa2g8a1,
+    'ZN_qLw4EhlxD0JXNU5': ZN_qLw4EhlxD0JXNU5,
+};
+
+export default g1ZoneToScript;
