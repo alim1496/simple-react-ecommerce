24f96fe3152fb6ac6f8977244958c0c765eb9956
Asma <asmaguedri0707@gmail.com>
2025-12-14 22:59:14 +0100

    Update homepage header text

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,7 +5,7 @@ const Banner: FC = () => (
   <div className="container mt-8 mx-auto px-4 md:flex font-lora">
     <img src="/banner.jpg" alt="banner" className="md:w-1/2" />
     <div className="bg-[#e3edf6] dark:bg-slate-600 dark:text-white md:w-1/2 flex flex-col items-center text-center justify-center p-4">
-      <h1 className="text-4xl font-bold mb-1">Don't miss the offer</h1>
+      <h1 className="text-4xl font-bold mb-1">Don't miss the offer!</h1>
       <h2 className="text-3xl font-semibold mb-4">Grab it now</h2>
       <Link
         to="/products"
