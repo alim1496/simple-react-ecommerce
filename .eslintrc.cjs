fcc54ee1f1e49b620679ac4d29eb666f0592354a
xfab-alim <alim@xfabric.ai>
2025-12-25 01:56:38 +0600

    add lint workflow (#14)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,13 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        'argsIgnorePattern': '^_',
+        'varsIgnorePattern': '^_',
+        'caughtErrorsIgnorePattern': '^_'
+      }
+    ],
   },
 }
